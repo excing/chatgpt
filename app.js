7f64af93f2eaa1704aa8d6de4f8dbd906c150b9e
excing <courage.exc@gmail.com>
2023-03-11 14:01:01 +0800

    Update: tts 默认播放本地语言

--- app.js
+++ app.js
@@ -424,15 +424,23 @@ const textToSpeech = async (text, options = {}) => {
 
   // Detect language using franc library
   const { franc } = await import("https://cdn.jsdelivr.net/npm/franc@6.1.0/+esm");
-  const lang = franc(text);
+  let lang = franc(text);
+  if (lang === "" || lang === "und") {
+    lang = navigator.language
+  }
+  if (lang === "cmn") {
+    lang = "zh-CN"
+  }
 
   // Get available voices and find the one that matches the detected language
   const voices = await new Promise(resolve => {
     const voices = synth.getVoices();
     resolve(voices);
   });
-  const voice = voices.find(v =>
-    langEq(v.lang, lang === 'cmn' ? 'zh-CN' : lang) && !v.localService);
+  const voice = voices.find(v => langEq(v.lang, lang) && !v.localService);
+  if (!voice) {
+    voice = voices.find(v => langEq(v.lang, navigator.language) && !v.localService);
+  }
 
   // Create a new SpeechSynthesisUtterance object and set its parameters
   const utterance = new SpeechSynthesisUtterance(text);
@@ -523,67 +531,3 @@ const speechToText = () => {
 
   recognition.start();
 }
-
-// https://mdn.github.io/dom-examples/web-speech-api/speech-color-changer/
-// var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-// var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
-// var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
-
-// var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
-
-// var recognition = new SpeechRecognition();
-// if (SpeechGrammarList) {
-//   // SpeechGrammarList is not currently available in Safari, and does not have any effect in any other browser.
-//   // This code is provided as a demonstration of possible capability. You may choose not to use it.
-//   var speechRecognitionList = new SpeechGrammarList();
-//   var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
-//   speechRecognitionList.addFromString(grammar, 1);
-//   recognition.grammars = speechRecognitionList;
-// }
-// recognition.continuous = false;
-// recognition.lang = 'en-US';
-// recognition.interimResults = false;
-// recognition.maxAlternatives = 1;
-
-// var diagnostic = document.querySelector('.output');
-// var bg = document.querySelector('html');
-// var hints = document.querySelector('.hints');
-
-// var colorHTML= '';
-// colors.forEach(function(v, i, a){
-//   console.log(v, i);
-//   colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
-// });
-// hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try ' + colorHTML + '.';
-
-// document.body.onclick = function() {
-//   recognition.start();
-//   console.log('Ready to receive a color command.');
-// }
-
-// recognition.onresult = function(event) {
-//   // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-//   // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-//   // It has a getter so it can be accessed like an array
-//   // The first [0] returns the SpeechRecognitionResult at the last position.
-//   // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-//   // These also have getters so they can be accessed like arrays.
-//   // The second [0] returns the SpeechRecognitionAlternative at position 0.
-//   // We then return the transcript property of the SpeechRecognitionAlternative object
-//   var color = event.results[0][0].transcript;
-//   diagnostic.textContent = 'Result received: ' + color + '.';
-//   bg.style.backgroundColor = color;
-//   console.log('Confidence: ' + event.results[0][0].confidence);
-// }
-
-// recognition.onspeechend = function() {
-//   recognition.stop();
-// }
-
-// recognition.onnomatch = function(event) {
-//   diagnostic.textContent = "I didn't recognise that color.";
-// }
-
-// recognition.onerror = function(event) {
-//   diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
-// }
