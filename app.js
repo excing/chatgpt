2d165fbcc6b40a132794505809c78491ec838c16
chshqiang <courage.exc@gmail.com>
2023-04-12 11:22:39 +0800

    Update: 支持 PWA, issue #4

--- app.js
+++ app.js
@@ -802,3 +802,13 @@ function detectStopRecording(stream, maxThreshold, callback) {
   };
   requestAnimationFrame(check);
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('./sw.js').then(function (registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      console.error('ServiceWorker registration failed: ', err);
+    });
+  });
+}
---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'my-pwa-app-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './app.js',
+    './main.css',
+    './prompts.json'
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
